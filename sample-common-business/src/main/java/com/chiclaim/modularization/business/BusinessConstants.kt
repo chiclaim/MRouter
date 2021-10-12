8d22f6b9e308ee7453c1fcafa0271e811d805217
chiclaim <chiclaim@gmail.com>
2021-10-12 17:36:35 +0800

    [update] kapt 增量编译

---
+++ sample-common-business/src/main/java/com/chiclaim/modularization/business/BusinessConstants.kt
@@ -0,0 +1,16 @@
+@file:JvmName("BusinessConstants")
+package com.chiclaim.modularization.business
+
+
+
+const val USER_INFO_FRAGMENT = "user/info/fragment"
+
+const val SOURCE_ORDER = "/source/order"
+
+const val ORDER_LIST_FRAGMENT = "/order/list/fragment"
+
+const val USER_ORDER_LIST = "/user/order/list"
+
+const val USER_LOGIN_FRAGMENT = "/user/login/fragment"
+
+
