97cf0e7921b34931854dd5beb254310e65daf18d
chiclaim <chiclaim@gmail.com>
2022-01-01 14:46:14 +0800

    [update] add comment

--- router-gradle-plugin/src/main/java/com/chiclaim/router/plugin/handler/IAttentionAnnotation.java
+++ router-gradle-plugin/src/main/java/com/chiclaim/router/plugin/handler/IAttentionAnnotation.java
@@ -5,6 +5,7 @@ public interface IAttentionAnnotation extends IAttention {
     /**
      * 是否是我关心的注解
      *
+     * @param desc description
      * @return boolean
      */
     boolean isAttentionAnnotation(String desc);
--- router-gradle-plugin/src/main/java/com/chiclaim/router/plugin/handler/IFieldVisitorHandler.java
+++ router-gradle-plugin/src/main/java/com/chiclaim/router/plugin/handler/IFieldVisitorHandler.java
@@ -8,6 +8,7 @@ public interface IFieldVisitorHandler {
      * 访问字段上的注解
      * @param desc eg. Lcom/chiclaim/router/annotation/Restore
      * @param visible 是否可见
+     * @param annotationBean 注解信息封装的bean
      */
     void visitFieldAnnotation(String desc, boolean visible, AnnotationBean annotationBean);
 
@@ -15,6 +16,7 @@ public interface IFieldVisitorHandler {
      * 访问注解中的成员
      * @param name 成员名
      * @param value 成员值
+     * @param annotationBean 注解信息封装的bean
      */
     void visitFieldAnnotationMember(String name, Object value,AnnotationBean annotationBean);
 
